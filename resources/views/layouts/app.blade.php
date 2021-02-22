1428d5b820403ca2294b92c9d2ad6fe2c7c4cd5b
shubhamvekariya <shubhamvekariya402@gmail.com>
2021-02-22 15:19:19 +0530

    project intial done

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>@yield('title')</title>
+
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    @yield('css')
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        @include('layouts.navigation')
+
+        <div id="page-wrapper" class="gray-bg">
+            @include('layouts.topnavbar')
+
+            @yield('content')
+
+
+        </div>
+    </div>
+
+    <!-- Mainly scripts -->
+    <script src="{!! asset('/js/jquery-3.1.1.min.js') !!}"></script>
+    <script src="{!! asset('/js/popper.min.js') !!}"></script>
+    <script src="{!! asset('/js/bootstrap.js') !!}"></script>
+    <script src="{!! asset('js/jquery.metisMenu.js') !!}"></script>
+    <script src="{!! asset('js/jquery.slimscroll.min.js') !!}"></script>
+
+    <!-- Custom and plugin javascript -->
+    <script src="{!! asset('js/inspinia.js') !!}"></script>
+    <script src="{!! asset('js/pace.min.js') !!}"></script>
+    @yield('script')
+
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,25 @@
-@extends('layout.app')
+@extends('layouts.app')
+@section('title')
+Empty Page
+@endsection
 
+@section('content')
+<div class="wrapper wrapper-content">
+    <div class="middle-box text-center animated fadeInRightBig">
+        <h3 class="font-bold">This is page content</h3>
+        <div class="error-desc">
+            You can create here any grid layout you want. And any variation layout you imagine:) Check out
+            main dashboard and other site. It use many different layout.
+            <br/><a href="index.html" class="btn btn-primary m-t">Dashboard</a>
+        </div>
+    </div>
+</div>
+<div class="footer">
+    <div class="float-right">
+        10GB of <strong>250GB</strong> Free.
+    </div>
+    <div>
+        <strong>Copyright</strong> Example Company &copy; 2014-2018
+    </div>
+</div>
+@endsection
