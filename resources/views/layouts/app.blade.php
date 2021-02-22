cbbb829b536557a3803b1c87ac27879131e88dd6
shubhamvekariya <shubhamvekariya402@gmail.com>
2021-02-22 15:31:43 +0530

    adding footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
 
             @yield('content')
 
-
+            @include('layouts.footer')
         </div>
     </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,12 +14,4 @@ Empty Page
         </div>
     </div>
 </div>
-<div class="footer">
-    <div class="float-right">
-        10GB of <strong>250GB</strong> Free.
-    </div>
-    <div>
-        <strong>Copyright</strong> Example Company &copy; 2014-2018
-    </div>
-</div>
 @endsection
