1fa11411e31518915f68ba2d72c66cdd24506c84
yagnesh987 <yagneshprajapati987@gmail.com>
2021-03-04 12:38:48 +0530

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,9 @@ Route::group(['middleware' => ['auth:society'] ], function(){
 
     Route::post('/society/rule', [SecretaryController::class,'add_rule'])->name('society.rule');
     Route::get('/society/all_rule', [SecretaryController::class,'show_rule'])->name('society.all_rule');
+    Route::get('/society/delete_rule/{id}', [SecretaryController::class,'delete_rule'])->name('society.delete_rule');
+    Route::get('/society/edit_rule/{id}', [SecretaryController::class,'edit_rule'])->name('society.edit_rule');
+    Route::put('/society/update_rule', [SecretaryController::class,'update_rule'])->name('society.update_rule');
 });
 
 Route::group(['middleware' => ['auth'] ], function(){
