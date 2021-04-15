491a3e47b00450116bfa3f1d81b5fb0340163c0a
shubhamvekariya <shubhamvekariya402@gmail.com>
2021-04-15 19:27:22 +0530

    fix/chnage logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="shubhamvekariya">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
     <title>@yield('title')</title>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     @stack('css')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
 	<meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="author" content="shubhamvekariya">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
     <title>ISocietyClub</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400" rel="stylesheet" />
 	<link href="{{ mix('/css/app.css') }}" rel="stylesheet">
